be989411e31cdc1939cba2667909d6a76cf5c62b
Greg Zaragoza <greg.g.zaragoza@gmail.com>
2021-11-04 19:31:02 -0500

    Can only access comment field on posts while logged in

--- public/js/post.js
+++ public/js/post.js
@@ -2,11 +2,14 @@ const newFormHandler = async (event) => {
     event.preventDefault();
 
     const content = document.querySelector('#new-comment').value.trim();
+    const post_id = window.location.toString().split('/')[
+		window.location.toString().split('/').length - 1
+	];
 
     if (content) {
         const response = await fetch(`/api/comments`, {
             method: 'POST',
-            body: JSON.stringify({ content }),
+            body: JSON.stringify({ post_id, content }),
             headers: {
                 'Content-Type': 'application/json',
             },
